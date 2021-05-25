e4d156ca0b7bc139da380dbc3772dd4fa1920811
EagleNerd <ugorjimiracle06@gmail.com>
2021-05-25 14:59:22 +0100

    images changed

--- recipe.js
+++ recipe.js
@@ -103,7 +103,7 @@ const recipeArr = ["49749", "1456", "224b14", "8cede5", "7a41bf", "e33f4f", "7de
 
  //get the search input value
 const getInput = (e) => {
-  // e.preventDefault();
+  e.preventDefault();
   const value = searchForm.querySelector("input").value;
   fetchData(value);
 
@@ -116,15 +116,25 @@ const getInput = (e) => {
 
  //fetch the search input endpoint
 const fetchData = async (search) => {
+  //validate the search input
   if (!recipeArray.includes(search)){
     ulDiv.innerHTML = `
-    <div class="error">
-    <p>No recipes found for your search. Please try again!</p>
+    <div class="alert-div">
+      <div>
+        <svg class="icon-alert-triangle">
+          <use xlink:href="alert.svg#icon-alert-triangle"></use>
+        /svg>
+      </div>
+      <h3>No recipes found for your search. Please try again!</h3>
   </div> 
     `;
     return;
   }
-  const loader = `<ion-icon name="reload" class="loader-icon"></ion-icon>`;
+  const loader =`
+      <svg class="loader-icon">
+        <use href="sync-solid.svg#icon-loader"></use>
+      </svg>
+  `;
   loaderDiv.innerHTML = loader;
   const url = `https://forkify-api.herokuapp.com/api/search?q=${search}`;
   const res = await fetch(url)
@@ -138,31 +148,32 @@ const fetchData = async (search) => {
  displayData(data.recipes);
 }
 
-
-
-
 //add html element for the recipes fetch result
 const displayData = data => {
-
-  ulDiv.innerHTML = '';
-
   pageDiv.innerHTML = `
   <button class="btn--inline pagination__btn--prev">
+    <svg class="search__icon">
+      <use xlink:href="arrow-left.svg#icon-arrow-left"></use>
+    </svg> 
     <span>Page 1</span>
   </button>
-
   <button class="btn--inline pagination__btn--next">
-    <span>Page 3</span>
+    <span>Page 3 </span>
+    <svg class="search__icon">
+      <use xlink:href="arrow-right.svg#icon-arrow-right"></use>
+    </svg> 
   </button>
-  
   `;
+
+  ulDiv.innerHTML = '';
+  
   let array = [];
 
   //loop through the fetch result to add elements
   data.map(result => {
         
-        let item = document.createElement("li")
-        item.classList.add("preview")
+        let item = document.createElement("li");
+        item.classList.add("preview");
         item.innerHTML = `
         <li class="preview">
             <a class="preview__link preview__link--active" href="#">
@@ -175,12 +186,13 @@ const displayData = data => {
               </div>
             </a>
           </li>
-        `
+        `;
         //listen for click on all the list element
-        item.addEventListener("click", getExtraData.bind(this, result.recipe_id))
-      
+        item.addEventListener("click", getExtraData.bind(this, result.recipe_id));
+
        //array for pagination
       array.push(item);
+      console.log(array);
   });
 
  // local variables for the pagination
@@ -248,7 +260,11 @@ const displayData = data => {
 //fetch the ids for the main display on click
 const getExtraData = async (id) => {
   console.log(id);
-  const loader2 = `<ion-icon name="reload-outline" class="spinner"></ion-icon>`;
+  const loader2 = `
+    <svg class="spinner">
+      <use href="sync.svg#icon-loader"></use>
+    </svg>
+  `;
   loaderDiv2.innerHTML = loader2;
   const recipeUrl = `https://forkify-api.herokuapp.com/api/get?rId=${id}`;
   const recipeRes = await fetch(recipeUrl)
@@ -264,102 +280,182 @@ const getExtraData = async (id) => {
 
 const displayRecipeData = (recipeData) => {
   
-  // console.log(recipeData);
+  console.log(recipeData);
    recipeDiv.innerHTML = '';
     
     let figure = document.createElement('figure');
     figure.classList.add('recipe__fig');
-    const figureImage = document.createElement('img');
-    figureImage.classList.add('recipe__img');
-    figureImage.setAttribute('alt', `${recipeData.title}`);
-    figureImage.src = `${recipeData.image_url}`;
-    const figureTitle = document.createElement('h1');
-    figureTitle.classList.add('recipe__title');
-    let figureSpan = document.createElement('span');
-    figureSpan.textContent = `${recipeData.title}`;
-    figureTitle.append(figureSpan);
-    figure.append(figureImage);
-    figure.append(figureTitle);
+    figure.innerHTML = `
+        <img src="${recipeData.image_url}" alt="${recipeData.title}" class="recipe__img" />
+        <h1 class="recipe__title">
+          <span>${recipeData.title}</span>
+        </h1>
+    `
+    // const figureImage = document.createElement('img');
+    // figureImage.classList.add('recipe__img');
+    // figureImage.setAttribute('alt', `${recipeData.title}`);
+    // figureImage.src = `${recipeData.image_url}`;
+    // const figureTitle = document.createElement('h1');
+    // figureTitle.classList.add('recipe__title');
+    // let figureSpan = document.createElement('span');
+    // figureSpan.textContent = `${recipeData.title}`;
+    // figureTitle.append(figureSpan);
+    // figure.append(figureImage);
+    // figure.append(figureTitle);
 
     recipeDiv.append(figure);
 
     const recipeDetails = document.createElement('div');
     recipeDetails.classList.add('recipe__details');
-  
-    const recipeInfo1 = document.createElement('div');
-    recipeInfo1.classList.add('recipe__info');
-    recipeInfo1.innerHTML = `
-    <ion-icon name="time-outline" class="recipe__info-icon"></ion-icon>
+    recipeDetails.innerHTML = `
+    <div class="recipe__info">
+    <svg class="recipe__info-icon">
+      <use xlinl:href="clock-regular.svg#icon-clock"></use>
+    </svg>
     <span class="recipe__info-data recipe__info-data--minutes">45</span>
-    <span class="recipe__info-text">minutes</span>`;
-    recipeDetails.append(recipeInfo1);
-
-    const RecipeInfo2 = document.createElement('div');
-    RecipeInfo2.classList.add('recipe__info');
-    RecipeInfo2.innerHTML =`
-    <ion-icon name="people-outline" class="recipe__info-icon></ion-icon>
+    <span class="recipe__info-text">minutes</span>
+  </div>
+  <div class="recipe__info">
+    <svg class="recipe__info-icon">
+      <use xlink:href="users-solid.svg#icon-users"></use>
+    </svg>
     <span class="recipe__info-data recipe__info-data--people">4</span>
-    <span class="recipe__info-text">servings</span>`;
-    recipeDetails.append(RecipeInfo2);
+    <span class="recipe__info-text">servings</span>
 
-    const recipeBTN = document.createElement('div');
-    recipeBTN.classList.add('recipe__info-buttons');
-    recipeBTN.innerHTML = `
-    <button class="btn--tiny btn--increase-servings">
-            <ion-icon name="remove-circle-outline"></ion-icon>
-    </button>
-    <button class="btn--tiny btn--increase-servings">
-            <ion-icon name="add-circle-outline"></ion-icon>
-    </button>`;
-    RecipeInfo2.append(recipeBTN);  
+    <div class="recipe__info-buttons">
+      <button class="btn--tiny btn--increase-servings">
+        <svg>
+          <use xlink:href="minus-circle.svg#icon-minus-circle"></use>
+        </svg>
+      </button>
+      <button class="btn--tiny btn--increase-servings">
+        <svg>
+          <use xlink:href="plus-circle.svg#icon-plus-circle"></use>
+        </svg>
+      </button>
+    </div>
+  </div>
+
+  <div class="recipe__user-generated">
+    <svg>
+      <use xlink:href="user-solid.svg#icon-user"></use>
+    </svg>
+  </div>
+  <button class="btn--round">
+    <svg class="">
+      <use xlink:href="heart.svg#icon-bookmark"></use>
+    </svg>
+  </button>
+    `;
+  
+  
+    // const recipeInfo1 = document.createElement('div');
+    // recipeInfo1.classList.add('recipe__info');
+    // recipeInfo1.innerHTML = `
+    // <ion-icon name="time-outline" class="recipe__info-icon"></ion-icon>
+    // <span class="recipe__info-data recipe__info-data--minutes">45</span>
+    // <span class="recipe__info-text">minutes</span>`;
+    // recipeDetails.append(recipeInfo1);
+
+    // const RecipeInfo2 = document.createElement('div');
+    // RecipeInfo2.classList.add('recipe__info');
+    // RecipeInfo2.innerHTML =`
+    // <ion-icon name="people-outline" class="recipe__info-icon></ion-icon>
+    // <span class="recipe__info-data recipe__info-data--people">4</span>
+    // <span class="recipe__info-text">servings</span>`;
+    // recipeDetails.append(RecipeInfo2);
+
+    // const recipeBTN = document.createElement('div');
+    // recipeBTN.classList.add('recipe__info-buttons');
+    // recipeBTN.innerHTML = `
+    // <button class="btn--tiny btn--increase-servings">
+    //         <ion-icon name="remove-circle-outline"></ion-icon>
+    // </button>
+    // <button class="btn--tiny btn--increase-servings">
+    //         <ion-icon name="add-circle-outline"></ion-icon>
+    // </button>`;
+    // RecipeInfo2.append(recipeBTN);  
 
 
-    const userGen = document.createElement('div');
-    userGen.classList.add('recipe__user-generated');
-    userGen.innerHTML = `
-    <button class="btn--round">
-      <ion-icon name="heart"></ion-icon>
-    </button>
-    `
-    recipeDetails.append(userGen);
+    // const userGen = document.createElement('div');
+    // userGen.classList.add('recipe__user-generated');
+    // userGen.innerHTML = `
+    // <button class="btn--round">
+    //   <ion-icon name="heart"></ion-icon>
+    // </button>
+    // `
+    // recipeDetails.append(userGen);
     recipeDiv.append(recipeDetails);
 
     const ingrDiv = document.createElement('div');
     ingrDiv.classList.add('recipe__ingredients');
-    const ingredients = `${recipeData.ingredients}`;
-    
-    ingrDiv.innerHTML = `
-      <h2 class="heading--2">Recipe ingredients</h2>`;
-      const ingrUl = document.createElement('ul');
-      ingrUl.classList.add('recipe__ingredient-list');
+    ingrDiv.innerHTML =`
+        ${recipeData.ingredients}
+    `;
+    // const ingredients = `${recipeData.ingredients}`;
+    // <h2 class="heading--2">Recipe ingredients</h2>
+    //       <ul class="recipe__ingredient-list">
+    //         <li class="recipe__ingredient">
+    //           <svg class="recipe__icon">
+    //             <use xlink:href="check-solid.svg#icon-check"></use>
+    //           </svg>
+    //           <div class="recipe__quantity">1000</div>
+    //           <div class="recipe__description">
+    //             <span class="recipe__unit">g</span>
+    //             pasta
+    //           </div>
+    //         </li>
 
-      const ingrList = document.createElement('li');
-      ingrList.classList.add('recipe__ingredient');
-      ingrList.append(ingredients);
-      ingrUl.append(ingrList);
-    
-      ingrDiv.append(ingrUl);
+    //         <li class="recipe__ingredient">
+    //           <svg class="recipe__icon">
+    //             <use xlink:href="check-solid.svg#icon-check"></use>
+    //           </svg>
+    //           <div class="recipe__quantity">0.5</div>
+    //           <div class="recipe__description">
+    //             <span class="recipe__unit">cup</span>
+    //             ricotta cheese
+    //           </div>
+    //         </li>
+              // </ul>
     recipeDiv.append(ingrDiv);
 
     const dirDiv = document.createElement('div');
     dirDiv.classList.add('recipe__directions');
     dirDiv.innerHTML = `
-      <h2 class="heading--2">How to cook it</h2>
-        <p class="recipe__directions-text">
-          This recipe was carefully designed and tested by
-          <span class="recipe__publisher">${recipeData.publisher}</span>. Please check out
-          directions at their website.
-        </p>
-        <a
-          class="btn--small recipe__btn"
-          href="${recipeData.publisher_url}"
-          target="_blank"
-        >
-          <span>Directions</span>
-          <svg >
-          <ion-icon name="arrow-forward-outline" class="search__icon"></ion-icon>
-        </a>
+    <h2 class="heading--2">How to cook it</h2>
+    <p class="recipe__directions-text">
+      This recipe was carefully designed and tested by
+      <span class="recipe__publisher">${recipeData.publisher}</span>. Please check out
+      directions at their website.
+    </p>
+    <a
+      class="btn--small recipe__btn"
+      href="${recipeData.publisher_url}"
+      target="_blank"
+    >
+      <span>Directions</span>
+      <svg class="search__icon">
+        <use xlink:href="arrow-right.svg#icon-arrow-right"></use>
+      </svg>
+    </a>
     `;
+    // dirDiv.innerHTML = `
+    //   <h2 class="heading--2">How to cook it</h2>
+    //     <p class="recipe__directions-text">
+    //       This recipe was carefully designed and tested by
+    //       <span class="recipe__publisher">${recipeData.publisher}</span>. Please check out
+    //       directions at their website.
+    //     </p>
+    //     <a
+    //       class="btn--small recipe__btn"
+    //       href="${recipeData.publisher_url}"
+    //       target="_blank"
+    //     >
+    //       <span>Directions</span>
+          
+    //       <ion-icon name="arrow-forward-outline" class="search__icon"></ion-icon>
+    //     </a>
+    // // `;
     recipeDiv.append(dirDiv);
 
     
